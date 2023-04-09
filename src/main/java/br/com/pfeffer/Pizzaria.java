189c8f7cad345d144d874033fa4fcaec0d1cb1ee
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-04-09 01:48:39 -0300

    Moved onlyNumbers()
    Moved onlyNumbers() to Utils class

--- src/main/java/br/com/pfeffer/Pizzaria.java
+++ src/main/java/br/com/pfeffer/Pizzaria.java
@@ -1,10 +1,6 @@
 package br.com.pfeffer;
 
 public class Pizzaria {
-    public static String onlyNumbers(String str) {
-        return str.replaceAll("\\D+", "");
-    }
-
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
--- src/main/java/br/com/pfeffer/core/utils/Utils.java
+++ src/main/java/br/com/pfeffer/core/utils/Utils.java
@@ -3,6 +3,11 @@ package br.com.pfeffer.core.utils;
 import java.util.Random;
 
 public class Utils {
+
+    public static String onlyNumbers(String str) {
+        return str.replaceAll("\\D+", "");
+    }
+
     public static int randomInteger(boolean positive) {
         if (positive) {
             return Math.abs(new Random().nextInt());
