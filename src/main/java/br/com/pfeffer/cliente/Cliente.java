5e392009ec9b402956b0ebdd5e1e3ca4155d5ee1
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-04-09 01:59:19 -0300

    Cliente basic attributes, getters and setters

--- src/main/java/br/com/pfeffer/cliente/Cliente.java
+++ src/main/java/br/com/pfeffer/cliente/Cliente.java
@@ -1,4 +1,27 @@
 package br.com.pfeffer.cliente;
 
 public class Cliente {
+    private String nome;
+    private String telefone;
+
+    public Cliente(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
