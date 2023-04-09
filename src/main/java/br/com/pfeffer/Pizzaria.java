f33bb501f3ffed2b73def992dfb4c16f3617b9dd
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-04-09 01:47:35 -0300

    Added onlyNumbers method for Utils

--- src/main/java/br/com/pfeffer/Pizzaria.java
+++ src/main/java/br/com/pfeffer/Pizzaria.java
@@ -1,6 +1,10 @@
 package br.com.pfeffer;
 
 public class Pizzaria {
+    public static String onlyNumbers(String str) {
+        return str.replaceAll("\\D+", "");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
