7178881d25e9bc36de24c553c956f5d74825a40c
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-04-09 02:01:19 -0300

    Endereco basic attributes, getters and setters

--- src/main/java/br/com/pfeffer/cliente/Endereco.java
+++ src/main/java/br/com/pfeffer/cliente/Endereco.java
@@ -1,4 +1,47 @@
 package br.com.pfeffer.cliente;
 
 public class Endereco {
+    private String logradouro;
+    private int numero;
+    private String complemento;
+    private String bairro;
+
+    public Endereco(String logradouro, int numero, String complemento, String bairro) {
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
 }
